be9f47092ca1d8bf3c2767bef4970029096d07b5
SanKaizyuu <imhappymow@gmail.com>
2025-10-22 03:54:36 +0900

    [Fix] ハードウェアコントローラーのインターフェイス化
    - DrawDisplayのインターフェイスクラスのIDisplayの追加
    - ExpPedalのインターフェイスクラスのIExpPedalの追加

---
+++ lib/interfaces/IDisplay.hpp
@@ -0,0 +1,37 @@
+#ifndef IDRAW_DISPLAY_HPP
+#define IDRAW_DISPLAY_HPP
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#define DEFAULT_ADC_ADDRESS 0x68
+
+class IDisplay {
+public:
+    virtual ~IDisplay() {}
+
+    // OLED等の初期化処理
+    virtual void begin(TwoWire* wireInstance,
+                       int SDA,
+                       int SCL,
+                       int screenWidth,
+                       int screenHeight,
+                       int screenAddress = DEFAULT_ADC_ADDRESS) = 0;
+
+    // 画面全体をクリア
+    virtual void clearDisplay() = 0;
+
+    // 中央に文字列を描画
+    virtual void drawCentreString(const String& buf) = 0;
+
+    // 中央に数値を描画
+    virtual void drawCentreNumber(int number) = 0;
+
+    // メニュー項目を描画
+    virtual void drawMenu(const String items[3],
+                          int cursorIndex,
+                          bool invertCursor,
+                          const String rightTexts[3]) = 0;
+};
+
+#endif // IDRAW_DISPLAY_HPP
